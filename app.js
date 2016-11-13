b2319625609022b37705fd57f9b5eee98415e682
sonyareznikova <sofya.reznikova@gmail.com>
2016-11-13 15:22:56 +0300

    reviews page ready with pagination

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var app = angular.module('hotelApp', []);
+        
         //creating hotel info
         app.factory('Hotel', function() {
             var hotels = [
@@ -7,126 +8,126 @@ var app = angular.module('hotelApp', []);
                 image: 'bower_components/img/pier.jpg',
                 rating: '8.5',
                 price: '4349',
-                stars: 3
+                stars: '3'
             },
             {
                 name: 'Hotel Sweet Hotel',
                 image: 'bower_components/img/car.jpg',
                 rating: '8.3',
                 price: '2329',
-                stars: 9
+                stars: '3'
             },
             {
                 name: 'Our Own Hotel',
                 image: 'bower_components/img/glassHat.jpg',
                 rating: '8.0',
                 price: '1389',
-                stars: 2
+                stars: '3'
             },
             {
                 name: 'Blackjack and Sluts',
                 image: 'bower_components/img/slippers.jpg',
                 rating: '8.5',
                 price: '4349',
-                stars: 4
+                stars: '3'
             },
             {
                 name: 'Not The Best Hotel',
                 image: 'bower_components/img/slipperCoconut.jpg',
                 rating: '8.3',
                 price: '2329',
-                stars: 6
+                stars: '3'
             },
             {
                 name: 'We Could Be Better',
                 image: 'bower_components/img/oceanPier.jpg',
                 rating: '8.0',
                 price: '1389',
-                stars: 5
+                stars: '3'
             },
             {
                 name: 'Blackjack and Sluts',
                 image: 'bower_components/img/hammock.jpg',
                 rating: '8.5',
                 price: '4349',
-                stars: 4
+                stars: '3'
             },
             {
                 name: 'Not The Best Hotel',
                 image: 'bower_components/img/maldives.jpg',
                 rating: '8.3',
                 price: '2329',
-                stars: 6
+                stars: '3'
             },
             {
                 name: 'We Could Be Better',
                 image: 'bower_components/img/pretty.jpg',
                 rating: '8.0',
                 price: '1389',
-                stars: 5
+                stars: '3'
             },
             {
                 name: 'Marijuana Legal',
                 image: 'bower_components/img/pier.jpg',
                 rating: '8.5',
                 price: '4349',
-                stars: 3
+                stars: '3'
             },
             {
                 name: 'Hotel Sweet Hotel',
                 image: 'bower_components/img/car.jpg',
                 rating: '8.3',
                 price: '2329',
-                stars: 9
+                stars: '3'
             },
             {
                 name: 'Our Own Hotel',
                 image: 'bower_components/img/glassHat.jpg',
                 rating: '8.0',
                 price: '1389',
-                stars: 2
+                stars: '3'
             },
             {
                 name: 'Blackjack and Sluts',
                 image: 'bower_components/img/slippers.jpg',
                 rating: '8.5',
                 price: '4349',
-                stars: 4
+                stars: '3'
             },
             {
                 name: 'Not The Best Hotel',
                 image: 'bower_components/img/slipperCoconut.jpg',
                 rating: '8.3',
                 price: '2329',
-                stars: 6
+                stars: '3'
             },
             {
                 name: 'We Could Be Better',
                 image: 'bower_components/img/oceanPier.jpg',
                 rating: '8.0',
                 price: '1389',
-                stars: 5
+                stars: '3'
             },
             {
                 name: 'Blackjack and Sluts',
                 image: 'bower_components/img/hammock.jpg',
                 rating: '8.5',
                 price: '4349',
-                stars: 4
+                stars: '3'
             },
             {
                 name: 'Not The Best Hotel',
                 image: 'bower_components/img/maldives.jpg',
                 rating: '8.3',
                 price: '2329',
-                stars: 6
+                stars: '3'
             },
             {
                 name: 'We Could Be Better',
                 image: 'bower_components/img/pretty.jpg',
                 rating: '8.0',
                 price: '1389',
-                stars: 5
+                stars: '3'
             }
             ];
 
@@ -139,7 +140,7 @@ var app = angular.module('hotelApp', []);
                 }
             };
         });
-        
+
         app.controller("PaginationCtrl", function($scope, Hotel) {
             $scope.itemsPerPage = 9;
             $scope.currentPage = 0;
@@ -160,47 +161,83 @@ var app = angular.module('hotelApp', []);
             return Math.ceil($scope.total/$scope.itemsPerPage);
             };
         });
-
-        app.controller = ('hotelCtrl', function() {
-            var hotel = {
-                name: 'Hotel Sweet Hotel',
-                image: 'bower_components/img/car.jpg',
-                rating: '8.3',
-                price: '2329',
-                stars: '9'
-            }
-            this.hotel = hotel;
-        });
-
-        app.controller = ('reviewCtrl', function() {
+        
+        app.factory('Review', function() {
             var reviews = [
             {
-                body: 'hehe that was a great stay, enjoyed it waaay too much',
-                locationRate: 6,
-                cleanlinessRate: 8,
-                serviceRate: 9,
-                valueForMoney: 7,
-                overallRating: 8.2,
-                name: 'Anna Wintour'
+                body: 'hehe that was a great stay, enjoyed it waaay too much', locationRate: '6', cleanlinessRate: '8', serviceRate: '9', 
+                valueForMoney: '7', overallRating: '8.2', name: 'Anna Wintour'
             },
             {
-                body: 'hehe that was a great stay, enjoyed it waaay too much',
-                locationRate: 6,
-                cleanlinessRate: 8,
-                serviceRate: 9,
-                valueForMoney: 7,
-                overallRating: 8.2,
-                name: 'Anna Wintour'
+                body: 'it was crazy', locationRate: '6', cleanlinessRate: '8', serviceRate: '9', 
+                valueForMoney: '7', overallRating: '8.2', name: 'Anna Wintour'
             },
             {
-                body: 'hehe that was a great stay, enjoyed it waaay too much',
-                locationRate: 6,
-                cleanlinessRate: 8,
-                serviceRate: 9,
-                valueForMoney: 7,
-                overallRating: 8.2,
-                name: 'Anna Wintour'
+                body: 'not much', locationRate: '6', cleanlinessRate: '8', serviceRate: '9', 
+                valueForMoney: '7', overallRating: '8.2', name: 'Anna Wintour'
+            },
+            {
+                body: 'hlol', locationRate: '6', cleanlinessRate: '8', serviceRate: '9', 
+                valueForMoney: '7', overallRating: '8.2', name: 'Anna Wintour'
+            },
+            {
+                body: 'hehe that was a great stay, enjoyed it waaay too much', locationRate: '6', cleanlinessRate: '8', serviceRate: '9', 
+                valueForMoney: '7', overallRating: '8.2', name: 'Anna Wintour'
+            },
+            {
+                body: 'boooobooo', locationRate: '6', cleanlinessRate: '8', serviceRate: '9', 
+                valueForMoney: '7', overallRating: '8.2', name: 'Anna Wintour'
+            },
+            {
+                body: 'hehe that was a great stay, enjoyed it waaay too much', locationRate: '6', cleanlinessRate: '8', serviceRate: '9', 
+                valueForMoney: '7', overallRating: '8.2', name: 'Anna Wintour'
+            },
+            {
+                body: 'hehe that was a great stay, enjoyed it waaay too much', locationRate: '6', cleanlinessRate: '8', serviceRate: '9', 
+                valueForMoney: '7', overallRating: '8.2', name: 'Anna Wintour'
+            },
+            {
+                body: 'hehe that was a great stay, enjoyed it waaay too much', locationRate: '6', cleanlinessRate: '8', serviceRate: '9', 
+                valueForMoney: '7', overallRating: '8.2', name: 'Anna Wintour'
+            },
+            {
+                body: 'waaay too drunk', locationRate: '6', cleanlinessRate: '8', serviceRate: '9', 
+                valueForMoney: '7', overallRating: '8.2', name: 'Anna Wintour'
+            },
+            {
+                body: 'hehe that was a great stay, enjoyed it waaay too much', locationRate: '6', cleanlinessRate: '8', serviceRate: '9', 
+                valueForMoney: '7', overallRating: '8.2', name: 'Anna Wintour'
             }
             ];
-            this.reviews = reviews;
+
+            return {
+                get: function(offset, limit) {
+                    return reviews.slice(offset, offset+limit);
+                },
+                total: function() {
+                    return reviews.length;
+                }
+            };
         });
+
+        app.controller("PaginationCtrlReviews", function($scope, Review) {
+            $scope.itemsPerPage = 3;
+            $scope.currentPage = 0;
+            $scope.total = Review.total();
+            $scope.pagedReviews = Review.get($scope.currentPage * $scope.itemsPerPage, $scope.itemsPerPage);
+            
+            $scope.loadMore = function() {
+            $scope.currentPage++;
+            var newReviews = Review.get($scope.currentPage*$scope.itemsPerPage,$scope.itemsPerPage);
+            $scope.pagedReviews = $scope.pagedReviews.concat(newReviews);
+            };
+
+            $scope.nextPageDisabledClass = function() {
+            return $scope.currentPage === $scope.pageCount()-1 ? "disabled" : "";
+            };
+
+            $scope.pageCount = function() {
+            return Math.ceil($scope.total/$scope.itemsPerPage);
+            };
+        });
+
